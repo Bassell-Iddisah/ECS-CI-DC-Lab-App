a418c8e0192918555cbe2383c611f8dfca8f44c3
Bassell-Iddisah <bbasssell16@gmail.com>
2025-09-18 11:47:51 +0000

    Choose to serve my index.html

---
+++ src/main/java/org/learner/todo/Controller/MainController.java
@@ -0,0 +1,17 @@
+package org.learner.todo.Controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+
+
+@Controller
+@RequestMapping("/")
+public class MainController {
+
+    @GetMapping()
+    public String index() {
+        return "redirect:/index.html";
+    }
+}
